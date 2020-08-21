4ad7e7c61452ed60c41d5a6468e910d9487b9f10
Joysing <1027577022@qq.com>
2020-08-21 17:09:27 +0800

    默认显示合同变更查询按钮

---
+++ Python/销售合同变更单列表.py
@@ -0,0 +1,16 @@
+﻿import clr
+clr.AddReference("Kingdee.BOS")
+clr.AddReference("Kingdee.BOS.App")
+clr.AddReference("Kingdee.BOS.Core")
+clr.AddReference("Kingdee.BOS.DataEntity")
+clr.AddReference("Kingdee.BOS.ServiceHelper")
+from Kingdee.BOS.Core.List.PlugIn import *
+from Kingdee.BOS.Core.List import *
+from Kingdee.BOS.Core.DynamicForm import *
+from Kingdee.BOS.App.Data import *
+from Kingdee.BOS.Core.Permission import *
+from Kingdee.BOS.ServiceHelper import *
+from System import *
+def AfterBindData(e):
+	#默认显示合同变更查询按钮
+	this.View.GetMainBarItem('tbChangeLog').Visible=True;
