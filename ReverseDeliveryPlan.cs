ca9ed08fac6ccccabe88a583841f21fbb2d70b9d
975623313 <44550246+975623313@users.noreply.github.com>
2020-08-21 09:47:19 +0800

    修改定时任务,改为欠数>0，下面4个join删掉

--- ReverseDeliveryPlan.cs
+++ ReverseDeliveryPlan.cs
@@ -36,12 +36,12 @@ namespace ZSKD.HUANGDING.Plugins
                     int entryId = Convert.ToInt32(EntryValue["Id"]);
                     string sql =
     "/*dialect*/update t_Pur_DeliveryPlanEntry set  FInStockQty = t7.FREALQTY,FOweQty=(t1.FQTY-t7.FREALQTY)  from t_Pur_DeliveryPlanEntry t1               " +
-    "join t_Pur_DeliveryPlanEntry_LK t2 on t1.FEntryID = t2.FEntryID and t2.FSTableName = 't_PUR_POOrderEntry'    " +
-    "join t_PUR_POOrderEntry t3 on t3.FID = t2.FSBillId   and t3.FEntryID=t2.FSID                   " +
-    "join T_PUR_RECEIVEENTRY_LK t4 on t4.FSBILLID = t3.FID and t3.FEntryID=t4.FSID and t4.FSTABLENAME = 't_PUR_POOrderEntry'              " +
-    "join T_PUR_RECEIVEENTRY t5 on t5.FENTRYID = t4.FENTRYID                                                      " +
-    "join T_STK_INSTOCKENTRY_LK t6 on t6.FSBILLID = t5.FID and t5.FEntryID=t6.FSID  and t6.FSTABLENAME = 'T_PUR_ReceiveEntry'              " +
-    "join T_STK_INSTOCKENTRY t7 on t7.FENTRYID = t6.FENTRYID   " +
+    " join t_Pur_DeliveryPlanEntry_LK t2 on t1.FEntryID = t2.FEntryID and t2.FSTableName = 't_PUR_POOrderEntry'    " +
+    " join t_PUR_POOrderEntry t3 on t3.FID = t2.FSBillId   and t3.FEntryID=t2.FSID                   " +
+    " join T_PUR_RECEIVEENTRY_LK t4 on t4.FSBILLID = t3.FID and t3.FEntryID=t4.FSID and t4.FSTABLENAME = 't_PUR_POOrderEntry'              " +
+    " join T_PUR_RECEIVEENTRY t5 on t5.FENTRYID = t4.FENTRYID                                                      " +
+    " join T_STK_INSTOCKENTRY_LK t6 on t6.FSBILLID = t5.FID and t5.FEntryID=t6.FSID  and t6.FSTABLENAME = 'T_PUR_ReceiveEntry'              " +
+    " join T_STK_INSTOCKENTRY t7 on t7.FENTRYID = t6.FENTRYID   " +
     "  where t7.FENTRYID = " + entryId;
                     DBUtils.Execute(Context, sql);
 
--- SHJH_UpdateDay.cs
+++ SHJH_UpdateDay.cs
@@ -13,18 +13,13 @@ using System.Threading.Tasks;
 namespace ZSKD.HUANGDING.Plugins
 {
     [HotUpdate]
-    [Description("定时任务：销售出库单汇总生成直接调拨单")]
+    [Description("定时任务：送货计划欠数大于0更新天数")]
     public class SHJH_UpdateDay : IScheduleService
     {
         public void Run(Context ctx, Schedule schedule)
         {
             string sql =
-              "/*dialect*/update t_Pur_DeliveryPlanEntry set  FEXPIRYDAYS=isnull(DATEDIFF(day,getdate(),t1.F_CGDDJHDATE),0)  from t_Pur_DeliveryPlanEntry t1               " +
-                    " join t_Pur_DeliveryPlanEntry_LK t2 on t1.FEntryID = t2.FEntryID and t2.FSTableName = 't_PUR_POOrderEntry'    " +
-                    " join t_PUR_POOrderEntry t3 on t3.FID = t2.FSBillId    and t3.FEntryID=t2.FSID                                                         " +
-                    " join T_PUR_RECEIVEENTRY_LK t4 on t4.FSBILLID = t3.FID and t3.FEntryID=t4.FSID and t4.FSTABLENAME = 't_PUR_POOrderEntry'              " +
-                    " join T_PUR_RECEIVEENTRY t5 on t5.FENTRYID = t4.FENTRYID    join T_PUR_Receive t6 on t6.FID=t5.FID " +
-                      "  where t1.FEXPIRYDAYS>0";
+              "/*dialect*/update t_Pur_DeliveryPlanEntry set FEXPIRYDAYS = isnull(DATEDIFF(day, getdate(), t1.F_CGDDJHDATE), 0)  from t_Pur_DeliveryPlanEntry t1 where t1.FOweQty > 0";
             DBUtils.Execute(ctx, sql);
         }
     }
