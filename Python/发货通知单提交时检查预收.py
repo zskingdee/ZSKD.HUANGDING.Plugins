3dbbfe144197f0c45cc321b6c52dc434ccb9f132
Joysing <1027577022@qq.com>
2020-11-13 16:55:28 +0800

    '完成收款条件'

--- Python/发货通知单提交时检查预收.py
+++ Python/发货通知单提交时检查预收.py
@@ -15,35 +15,29 @@ from Kingdee.BOS.Core.Permission import *
 from Kingdee.BOS.ServiceHelper import *
 from System import *
 def BeforeExecuteOperationTransaction(e):
-	#发货通知单提交时检查预收
-	if this.FormOperation.Operation=="Submit":
-	#{
-		for dataobj in e.SelectedRows:
-		#{
-			FRECEIPTCONDITIONID=dataobj["FRECEIPTCONDITIONID"]
-			if FRECEIPTCONDITIONID<>None:
-			#{
-				ReceiptConditionControl=dataobj["FRECEIPTCONDITIONID"]["F_HDIN_SpecialApproval"] #收款条件是否勾选预收特批控制
-				if ReceiptConditionControl==True:
-				#{
-					sql="/*dialect*/select distinct t2.FBILLNO from T_SAL_DELIVERYNOTICEENTRY t1 join T_SAL_ORDER t2 on t1.FORDERNO=t2.FBILLNO "
-					sql=sql+" join T_SAL_ORDERPLAN t3 on t2.FID=t3.FID and t3.FNEEDRECADVANCE=1 and t3.FRECADVANCEAMOUNT>t3.FRECAMOUNT "
-					sql=sql+" join T_SAL_DELIVERYNOTICE t4 on t4.FID=t1.FID and t4.F_HDIN_SpecialApproval<>1 "
-					sql=sql+" where t1.FID="+str(dataobj["ID"])
-					#raise Exception("提示信息！"+str(dataobj["FRECEIPTCONDITIONID"]["Number"]))
-					rows=DBUtils.ExecuteDynamicObject(this.Context,sql);
-					SEOrders=""
-					if rows.Count>0:
-						for row in rows:
-							SEOrders=SEOrders+row["FBILLNO"]+","
-					if SEOrders!="":
-					#{
-						e.Cancel=True
-						e.CancelMessage="销售订单"+SEOrders+"的收款计划是预收类型，但实收金额小于应收金额，提交失败。"
-					#}
-				#}
-			#}
+    #发货通知单提交时检查预收
+    if this.FormOperation.Operation=="Submit":
+    #{
+        for dataobj in e.SelectedRows:
+        #{
+            sql="/*dialect*/select distinct t2.FBILLNO from T_SAL_DELIVERYNOTICEENTRY t1 join T_SAL_ORDER t2 on t1.FORDERNO=t2.FBILLNO "
+            sql=sql+" join (select sum(FRECADVANCEAMOUNT) as FRecAdvanceAmount,sum(FRecAmount) as FRecAmount,FID from t_sal_OrderPlan "
+            sql=sql+" where FControlSend='SEND' group by FID) t3 on t2.FID=t3.FID and t3.FRECADVANCEAMOUNT<>t3.FRECAMOUNT " #实收不等于应收时
+            sql=sql+" and t3.FID not in(select FID from t_sal_orderPlan c where c.FControlSend='SEND' and c.FISOUTSTOCKBYRECAMOUNT='1') " #按实际预收控制发货 未勾选
+            sql=sql+" join T_SAL_DELIVERYNOTICE t4 on t4.FID=t1.FID and t4.F_HDIN_SpecialApproval<>1 "
+            sql=sql+" where t1.FID="+str(dataobj["ID"])
+            #raise Exception("提示信息！"+str(dataobj["FRECEIPTCONDITIONID"]["Number"]))
+            rows=DBUtils.ExecuteDynamicObject(this.Context,sql);
+            SEOrders=""
+            if rows.Count>0:
+                for row in rows:
+                    SEOrders=SEOrders+row["FBILLNO"]+","
+            if SEOrders!="":
+            #{
+                e.Cancel=True
+                e.CancelMessage="销售订单"+SEOrders+" 通过预收控制发货，需要预收的实收金额之和等于应收金额之和，提交失败。"
+            #}
 		#}
 	#}
 def OnPreparePropertys(e):
-	e.FieldKeys.Add("FRECEIPTCONDITIONID");
+    e.FieldKeys.Add("FRECEIPTCONDITIONID");
---
+++ Python/销售订单提交操作.py
@@ -0,0 +1,53 @@
+﻿import clr
+clr.AddReference("Kingdee.BOS")
+clr.AddReference("Kingdee.BOS.App")
+clr.AddReference("Kingdee.BOS.Core")
+clr.AddReference("Kingdee.BOS.DataEntity")
+clr.AddReference("Kingdee.BOS.ServiceHelper")
+clr.AddReference("Kingdee.K3.SCM.App.Sal.ServicePlugIn")
+from Kingdee.BOS import *
+from Kingdee.BOS.Util import *
+from Kingdee.BOS.Core.Bill import *
+from Kingdee.BOS.Core.DynamicForm import *
+from Kingdee.BOS.Core.DynamicForm.PlugIn import *
+from Kingdee.BOS.Core.DynamicForm.PlugIn.ControlModel import *
+from Kingdee.BOS.App.Data import *
+from Kingdee.BOS.Core.Permission import *
+from Kingdee.BOS.ServiceHelper import *
+from Kingdee.K3.SCM.App.Sal.ServicePlugIn.SaleOrder import *
+import Kingdee.K3.SCM.App.Sal.ServicePlugIn.SaleOrder as ServSalOrder
+from System import *
+
+def OnAddValidators(e):
+    for dataobj in e.DataEntities:
+        FID=dataobj["ID"]
+        F_HDIN_SpecialApproval=dataobj["F_Hdin_CheckBox"]
+        sql="/*dialect*/select distinct F_Hdin_CheckBox from T_SAL_ORDER where FID= "+str(FID)
+        rows=DBUtils.ExecuteDynamicObject(this.Context,sql);
+
+        if rows.Count>0:
+            F_HDIN_SpecialApproval=int(rows[0]["F_Hdin_CheckBox"])
+
+        if F_HDIN_SpecialApproval==0:
+            item=ServSalOrder.SubmitValidator()
+            item.AlwaysValidate = True
+            item.EntityKey = "FBillHead"
+            e.Validators.Add(item);
+            break;
+        #F_HDIN_SpecialApproval  F_HDIN_CHECKBOX
+
+def OnPreparePropertys(e):
+    e.FieldKeys.Add("FPrice");
+    e.FieldKeys.Add("FTaxPrice");
+    e.FieldKeys.Add("FPriceUnitId");
+    e.FieldKeys.Add("FTaxNetPrice");
+    e.FieldKeys.Add("FLimitDownPrice");
+    e.FieldKeys.Add("FIsIncludedTax");
+    e.FieldKeys.Add("FMaterialId");
+    e.FieldKeys.Add("FUnitId");
+    e.FieldKeys.Add("FSettleCurrId");
+    e.FieldKeys.Add("FIsFree");
+    e.FieldKeys.Add("FPriceCoefficient");
+    e.FieldKeys.Add("FSysPrice");
+    e.FieldKeys.Add("F_Hdin_CheckBox");
+
